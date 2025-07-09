bb75fcce5c76e10916da4e2247df7569e4b82e7e
Krishnakhandelwal123 <141343323+Krishnakhandelwal123@users.noreply.github.com>
2025-07-09 16:45:32 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 3000;
 if(process.env.NODE_ENV==='production'){
   app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-  app.get('/{*any}',(req,res)=>{
+  app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
   })
 }
