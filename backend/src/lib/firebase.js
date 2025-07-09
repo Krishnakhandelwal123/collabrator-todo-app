1cfbdf05baaedee00b0cbb2c6ccd366921bcb794
Krishnakhandelwal123 <141343323+Krishnakhandelwal123@users.noreply.github.com>
2025-07-09 15:34:59 +0530

    final

--- backend/src/lib/firebase.js
+++ backend/src/lib/firebase.js
@@ -3,7 +3,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 try {
-  const serviceAccount = require('./serviceAccountKey.json');
+  const serviceAccount = require('E:/major mern projects/collabrator todo app/backend/src/lib/serviceAccountKey.json');
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
